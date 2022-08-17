e605656880f463abce3d7269056c98a029a456bf
David O'Brien <ddrobrien@icloud.com>
2022-08-17 19:14:57 +0000

    errors not fixed

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-print("Please enter sales data from the last market")
+    print("Please enter sales data from the last market")
 print("Data should be six numbers, seperated by commas.")
 print("Example: 10,20,30,40,50,60\n")
 
@@ -33,12 +33,12 @@ def validate_data(values):
     Raise ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
-try:
-    if len(values) != 6:
-        raise ValueError(
-            f"Exactly 6 values required, you provided {len(values)}"
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
         )
-except ValueError as e:
-    print(f"Invalid data: {e}, please try again. \n")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again. \n")
 
-get_sales_data()
+    get_sales_data()
